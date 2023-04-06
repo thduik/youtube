f7a51ad67695cb52c2e6dbb3651ea9fda911318d
thduik <linhn0478@gmail.com>
2023-04-06 21:22:24 +0700

    before rebase

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
+  },
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..'],
+    },
   }
 })
 
